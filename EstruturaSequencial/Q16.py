89c06247a9c48177438e875bcf2716002dca1787
amandakarolline <amandakarolline@hotmail.com>
2022-02-04 16:27:17 -0300

    Inclusão da questão 16.

---
+++ EstruturaSequencial/Q16.py
@@ -0,0 +1,27 @@
+# 16) Faça um programa para uma loja de tintas.
+# O programa deverá pedir o tamanho em metros quadrados da área a ser pintada.
+# Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados
+# e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00.
+# Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
+
+import math
+
+
+def leia_numero():
+    while True:
+        try:
+            numero = float(input('Digite a área em m² que deseja pintar: '))
+        except ValueError:
+            print("Você não digitou um número.")
+        else:
+            break
+
+    return numero
+
+
+area = leia_numero()
+litros = math.ceil(area/3)
+latas = math.ceil(litros/18)
+preco = latas*80
+
+print(f'Será preciso comprar {latas} latas de tinta que custarão R$ {preco},00')
--- EstruturaSequencial/Q8.py
+++ EstruturaSequencial/Q8.py
@@ -1,7 +1,7 @@
 # 8) Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
 # Calcule e mostre o salarioBruto do seu salário no referido mês.
 
-def leia_numero(valor = 'numero'):
+def leia_numero(valor='numero'):
     while True:
         try:
             numero = float(input(f'Digite o {valor}: '))
