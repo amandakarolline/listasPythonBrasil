57df89691282ddf1d9488427087a281f3c6fa87d
amandakarolline <amandakarolline@hotmail.com>
2022-02-04 03:49:12 -0300

    Inclusão questão 8.

---
+++ EstruturaSequencial/Q8.py
@@ -0,0 +1,20 @@
+# 8) Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+# Calcule e mostre o total do seu salário no referido mês.
+
+def leia_numero(valor = 'numero'):
+    while True:
+        try:
+            numero = float(input(f'Digite o {valor}: '))
+        except ValueError:
+            print("Você não digitou um número.")
+        else:
+            break
+
+    return numero
+
+
+dinheiro = leia_numero('salario por hora')
+horas = leia_numero('número de horas por mês')
+total = dinheiro*horas
+
+print(f'Você recebe {total} por mês.')
