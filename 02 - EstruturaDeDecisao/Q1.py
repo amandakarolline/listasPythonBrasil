d8c5a0a1d3b32b42deb6a308bbb9239fc6ac5cf1
amandakarolline <amandakarolline@hotmail.com>
2022-02-04 20:49:02 -0300

    Criação do diretório da lista 2. Renomeação dos diretórios. Inclusão da questão 1 da lista 2.

---
+++ 02 - EstruturaDeDecisao/Q1.py
@@ -0,0 +1,19 @@
+# 1) Faça um Programa que peça dois números e imprima o maior deles.
+
+def leia_numero():
+    while True:
+        try:
+            numero = float(input('Digite um número: '))
+        except ValueError:
+            print("Você não digitou um número.")
+        else:
+            break
+
+    return numero
+
+
+numero1 = leia_numero()
+numero2 = leia_numero()
+maior = max(numero1,numero2)
+
+print(f'O maior número é {maior}')
