bbf9984c689cf78ac8225f0d072912a12b2d811b
amandakarolline <amandakarolline@hotmail.com>
2022-02-04 05:05:00 -0300

    Inclusão questão 13.

---
+++ EstruturaSequencial/Q13.py
@@ -0,0 +1,33 @@
+# 13) Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal,
+# utilizando as seguintes fórmulas:
+# a) Para homens: (72.7*h) - 58
+# b) Para mulheres: (62.1*h) - 44.7
+
+def leia_numero():
+    while True:
+        try:
+            numero = float(input('Digite sua altura: '))
+        except ValueError:
+            print("Você não digitou um número.")
+        else:
+            break
+
+    return numero
+
+
+h = leia_numero()
+
+while True:
+    sexo = input('Digite o F para feminino e M para masculino: ')
+    sexo = sexo.upper()
+
+    if sexo == 'M':
+        peso = (72.7*h)-58
+        break
+    elif sexo == 'F':
+        peso = (62.1*h) - 44.7
+        break
+    else:
+        continue
+
+print(f'O seu peso ideal é {peso}')
