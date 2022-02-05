dcb0d6a41cb39c5e96a44c954030a5bee84cd3bc
amandakarolline <amandakarolline@hotmail.com>
2022-02-05 00:55:56 -0300

    Inclusão da questão 8 da lista 2.

---
+++ 02 - EstruturaDeDecisao/Q8.py
@@ -0,0 +1,21 @@
+# 8) Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar,
+# sabendo que a decisão é sempre pelo mais barato.
+
+def leia_numero():
+    while True:
+        try:
+            numero = float(input('Digite um preço: '))
+        except ValueError:
+            print("Você não digitou um número.")
+        else:
+            break
+
+    return numero
+
+
+numero1 = leia_numero()
+numero2 = leia_numero()
+numero3 = leia_numero()
+menor = min(numero1, numero2, numero3)
+
+print(f'O menor preço é {menor:,.2f}')
